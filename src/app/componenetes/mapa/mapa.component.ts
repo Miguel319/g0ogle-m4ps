7fada81c2b5335751993b749aed314b26e7bd6b5
Miguel319 <miguelp90.319@gmail.com>
2019-10-28 12:05:00 -0400

    Creación y persistencia de marcadores

--- src/app/componenetes/mapa/mapa.component.ts
+++ src/app/componenetes/mapa/mapa.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Marcador } from "../../classes/marcador.class";
 
 @Component({
-  selector: 'app-mapa',
-  templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
+  selector: "app-mapa",
+  templateUrl: "./mapa.component.html",
+  styleUrls: ["./mapa.component.css"]
 })
 export class MapaComponent implements OnInit {
+  marcadores: Marcador[] = [];
+
   lat: number = 51.678418;
   lng: number = 7.809007;
-  
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit() {
+    this.marcadores = JSON.parse(
+      localStorage.getItem("Marcadores")
+    ) as Marcador[];
   }
 
+  agregarMarcador(evento: Event) {
+    const coords: { lat: number; lng: number } = evento["coords"];
+    const nuevoEvento = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(nuevoEvento);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem("Marcadores", JSON.stringify(this.marcadores));
+  }
 }
